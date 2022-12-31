88b8a7b814476cf69a40167b397ce092a5d45360
Shaheed-N <username@example.com>
2022-12-31 11:57:43 +0400

    commit grid

--- SmartHome/Core/SecondView/GridElement.swift
+++ SmartHome/Core/SecondView/GridElement.swift
@@ -44,24 +44,16 @@ CustomToggle()
                     .font(.custom(Fonts.Poppins.medium, size: 10))
                 }
                 .padding(.top , -110)
-
-            }
-
-                  }
-
-
+              }
+          }
         }
     }
-
 }
-
-
 struct GridElement_Previews: PreviewProvider {
     static var previews: some View {
       GridElement()
     }
 }
-
 struct CustomToggle: View {
   @State private var flag = true
   let gradient = Gradient(colors: [Color.theme.purpleColor, .white])
@@ -96,23 +88,19 @@ struct CustomToggle: View {
                     .frame(width: 15, height: 15)
                     .offset(x: flag ? -25 : -10)
                     .padding(.leading, 35)
-
-
-                }}
+                }
+              }
               CircleNeumo()
             }
             .padding(.top , -40)
-
           }
         }
         else {
           ZStack {
-
             RoundedRectangle(cornerRadius: 20)
               .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 0.60, green: 0.30, blue: 0.97), Color(red: 0.15, green: 0.09, blue: 0.16)]), startPoint: .topLeading, endPoint: .bottomTrailing))
               .frame(width: 142, height: 167)
               .overlay(RoundedRectangle(cornerRadius: 20).stroke(LinearGradient(gradient: Gradient(colors: [Color.white, Color(red: 0, green: 0, blue: 0, opacity: 0)]), startPoint: .top, endPoint: .bottom), lineWidth: 1))
-
             VStack {
               HStack(spacing:30) {
                 Text("ON")
@@ -125,28 +113,23 @@ struct CustomToggle: View {
                     .foregroundColor(Color("toggle"))
                     .frame(width: 35, height: 20)
 
-
                   Circle()
                     .foregroundColor(flag ? Color("circletoggle") : .white)
                     .frame(width: 15, height: 15)
                     .offset(x: flag ? -25 : -10)
                     .padding(.leading, 35)
-
-
-                }}
+                }
+              }
               Circle()
                 .fill( RadialGradient(gradient: gradient, center: .center, startRadius: 12, endRadius:65))
                 .frame(width: 40, height: 40)
             }
             .padding(.top , -40)
-
           }
         }
-      }
-        )
-
-        }
-        }
+      })
+    }
+}
 
 
 
